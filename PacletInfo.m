6901f83efd8f22b3fce147189ae512953facb11f
Kuba Podkalicki <kuba.pod@gmail.com>
2019-12-04 09:17:32 +0100

    files init

---
+++ PacletInfo.m
@@ -0,0 +1,6 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "WLForms", Version -> "0.0.1", MathematicaVersion -> "11.0+", 
+ Description -> "Alternative to FormObjects, focused on desktop Mathematica \
+rather than Wolfram Cloud.", 
+ Creator -> "Kuba Podkalicki (kuba.pod@gmail.com)", 
+ Extensions -> {{"Kernel", Context -> "WLForms`"}, {"FrontEnd"}}]
---
+++ WLForms.m
@@ -0,0 +1,47 @@
+
+(* ::Chapter:: *)
+(* Metadata*)
+
+
+(* Mathematica Package *)
+
+(* :Title: WLForms *)
+(* :Context: WLForms` *)
+(* :Author: Kuba Podkalicki (kuba.pod@gmail.com) *)
+(* :Date: Wed 4 Dec 2019 09:17:03 *)
+
+(* :Keywords: *)
+(* :Discussion: *)
+
+
+
+(* ::Chapter:: *)
+(* Begin package*)
+
+
+BeginPackage["WLForms`"];
+
+Unprotect["`*", "`*`*"]
+ClearAll["`*", "`*`*"]
+
+(* Needs and exported symbols here *)
+
+Begin["`Private`"];
+
+
+
+(* ::Chapter:: *)
+(* Implementation code*)
+
+
+(*implementation goes here*)
+
+
+
+(* ::Chapter:: *)
+(* End package*)
+
+
+End[];
+
+EndPackage[];
