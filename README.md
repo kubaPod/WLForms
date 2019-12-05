57ddc36dd06df106283588cae37351f5814c86c0
Kuba Podkalicki <kuba.pod@gmail.com>
2019-12-05 11:41:56 +0100

    code init

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
+!!Very raw state!!
+
 # WLForms
 
 Alternative to FormObjects, focused on desktop Mathematica rather than Wolfram Cloud.
+
+
+## Quick intro
+
+The idea is to borrow as much as possible from FormObjects/FormFunctions but to adapt it for desktop gui creators' sake.
+
+There are a few problems with FormObjects in MMA, which otherwise work well withing the Cloud environment, that prompted me to create it.
+
+- extracting values
+
+  `Setting[formObject] @ "x"` is not very handy, 
+  
+  ``WLForms` `` allows you to do `form["x"]`. Or `form["State"]` to get them all.
+
+- setting values
+
+  `formObject @ <|"x" -> 1|>` is not very idiomatic and together with `Setting` it makes creating own field controllers weird
+   
+   ``WLForms` `` allows you to do `form["x"] = 1`
+   
+- custom field controllers
+
+  While `formObject["x"]` automatically creating a controller is nice but it is either this or you need to assemble your controller from scratch.
+  
+  ``WLForms` `` allows you to do `FieldController @ whatever[Dynamic[form@"x"]]` and the `FieldController` wrapper will take care of labeling it in case of interpretation failure. Future releases will bring more features.
+  
+  I am also not a fan of warnings displayed as a part of the same layer of layout because it moves/changes the structure of everything so ``WLForms` `` uses `AttachedCell` to display warnings.
+  
+  
+For a small example please see [Wiki](https://github.com/kubaPod/WLForms/wiki).     
+   
+
+
+
+## Installation
+ 
+### Manual
+ 
+   Go to 'releases' tab and download appropriate .paclet file.
+    
+   Run `PacletInstall @ path/to/the.paclet` file
+   
+### Via ``MPM` ``
+   
+If you don't have ``MPM` `` yet, run:
+   
+```Mathematica
+Import[" https://raw.githubusercontent.com/kubapod/mpm/master/install.m"]
+```   
+
+and then:
+   
+```Mathematica
+Needs @ "MPM`"     
+MPMInstall["kubapod", "wlforms"]
+```
+
--- WLForms/WLForms.m
+++ WLForms/WLForms.m
@@ -22,10 +22,15 @@
 
 BeginPackage["WLForms`"];
 
-Unprotect["`*", "`*`*"]
-ClearAll["`*", "`*`*"]
+  Needs @ "GeneralUtilities`";
 
-(* Needs and exported symbols here *)
+  Unprotect["`*", "`*`*"]
+  ClearAll["`*", "`*`*"]
+
+  Form;
+  FieldController;
+
+
 
 Begin["`Private`"];
 
@@ -39,69 +44,117 @@ Begin["`Private`"];
 
 FormQ = MatchQ[_Form];
 
+$nonFieldKeys = {"ValidQ"};
+
+Form[spec_][key_]:= spec // Query[key, "Input"]
+Form[spec_][key_, "Controllers"]:=  Lookup[ spec[[key]], "Controllers", {}]
+Form[spec_][key : Alternatives @@ $nonFieldKeys]:=  Lookup[ spec, key]
+
+Form[spec_]["State"]:= KeyDrop[spec, $nonFieldKeys][[All, "Value"]]
+
 formMutationHandler//Attributes={HoldAllComplete};
 
-formMutationHandler[Set[(m_Symbol?FormQ)[field_],val_]]:=Module[
+formMutationHandler[Set[(m_?FormQ)[field_],val_]]:=Module[
  {spec=m[[1]], new, isValid, wasValid}
+
 , new = FieldInterpreter[spec[[field, "Interpreter"]]] @ val
 ; spec[[field, "Input"]] = val
 ; spec[[field, "Value"]] = new
 
 ; wasValid = Lookup[spec[[field]], "ValidQ", True]
-; isValid = MatchQ[_Failure] @ new
+; isValid = Not @ MatchQ[_Failure] @ new
 
 ; spec[field, "ValidQ" ] = isValid
-; If[Echo[wasValid && Not @ isValid], FieldControllerMark[m[field], new] ]
-; If[Not @ wasValid && isValid, FieldControllerUnmark[m[field]] ]
 
-; spec["ValidQ"] = spec // Query[ FreeQ[_Failure] @* KeyDrop["ValidQ"], "Value"]
+; If[ wasValid && !isValid, FieldControllerMark[m[field], new] ]
+; If[!wasValid &&  isValid, FieldControllerUnmark[m[field]] ]
+
+; spec["ValidQ"] = spec // Query[ FreeQ[_Failure | _Missing] @* KeyDrop[$nonFieldKeys], "Value"]
 ; m = Form @ spec
 ; new
 ]
 
 
 FieldControllerMark // Attributes = {HoldAllComplete};
-FieldControllerMark[form_Symbol?FormQ[field_], failure_]:= #["Mark", failure]& /@ Lookup[form[[1, field]], "controllers", {}]
-
+FieldControllerMark[form_?FormQ[field_], failure_]:= #["Mark", failure]& /@ form[field, "Controllers"]
 
 FieldControllerUnmark // Attributes = {HoldAllComplete};
-FieldControllerUnmark[form_Symbol?FormQ[field_]]:= #["Unmark"]& /@ Lookup[form[[1, field]], "controllers", {}]
+FieldControllerUnmark[form_?FormQ[field_]]:= #["Unmark"]& /@ form[field, "Controllers"]
 
 
-FieldInterpreter[spec_String]:=Interpreter[spec]
+FieldInterpreter[spec_]:=Interpreter[spec]
 FieldInterpreter[i_Interpreter]:=i
 
 
 Language`SetMutationHandler[Form,formMutationHandler];
 
 
-Form /: Form[spec_][key_]:= spec // Query[key, "Input"]
 
+FieldController::usage = "FieldController @ controller_[Dynamic[form@field],___] wraps the controller in the "<>
+  "box structure that 'subscribes' to the form's field changes and displays failure strings "<>
+  "when an invalid input is set for form@field.
 
-FieldController[con:_[Dynamic[sym_[field_], ___],___]]:=DynamicModule[
-  {controllerRoot,style}
-, Style[con, Background -> Dynamic @ style]
+Supported forms:
+
+FieldController @ standardController @ Dynamic @ form @ \"x\", where standardController can e.g. be an InputField
+
+FieldController[ Dynamic[field@\"x\"], nonStandardController_]
+
+FieldController[Dynamic[field@\"x\"] ] @ controller_
+"
+
+FieldController // Attributes = {HoldFirst}
+
+FieldController[spec : Dynamic[_Symbol?FormQ[_], ___]]:=Function[controller, FieldController[spec, controller] ]
+
+FieldController[con:_[spec:Dynamic[_Symbol?FormQ[_], ___],___]]:=FieldController[spec, con]
+
+FieldController[Dynamic[form_Symbol?FormQ[field_], ___], controller_] := DynamicModule[
+  {controllerRoot, thisBox}
+, controller
 , Initialization :> (
-    FieldControllerAdd[sym[field], controllerRoot]
-  ; controllerRoot["Mark", failure_]:=style= Red
-  ; controllerRoot["Unmark"]:=style= Black
+    thisBox = EvaluationBox[]
+
+  ; FieldControllerAdd[form[field], controllerRoot]
+
+
+  ; controllerRoot["Mark", failure_]:= (
+      controllerRoot["Unmark"]
+    ; controllerRoot["warningCell"] = AddWarning[thisBox, failure]
+    )
+  ; controllerRoot["Unmark"]:= (
+      RemoveWarning[controllerRoot["warningCell"]]
+    ; controllerRoot["warningCell"] = {}
+    )
   )
-, Deinitialization :> FieldControllerRemove[sym[field], controllerRoot]
+, Deinitialization :> FieldControllerRemove[form[field], controllerRoot]
+]
+
+AddWarning[box_BoxObject, msg_Failure]:= AddWarning[box, FailureString @ msg];
+AddWarning[box_BoxObject, msg_]:= FrontEndExecute @ FrontEnd`AttachCell[
+  box
+, Cell[ BoxData @ ToBoxes @ msg, "Message"]
+, {Automatic, {Left, Bottom}}
+, {Left, Top}
+, "ClosingActions" -> {"EvaluatorQuit"}
 ]
 
 
+
+RemoveWarning[cell_CellObject]:= NotebookDelete @ cell;
+
 FieldControllerAdd // Attributes = {HoldAllComplete};
-FieldControllerAdd[form_Symbol?FormQ[field_], root_]:= Module[{spec}
+FieldControllerAdd[form_?FormQ[field_], root_]:= Module[{spec}
 , spec = form[[1]] 
-; spec[[field, "Controllers"]] = Union @ Flatten @  {Lookup[spec[field], "Controllers", {}], root}
+; spec[[field, "Controllers"]] = Union @ Flatten @  {form[field, "Controllers"], root}
 ; form = Form @ spec
 ]
 
 
 FieldControllerRemove // Attributes = {HoldAllComplete};
-FieldControllerRemove[form_Symbol?FormQ[field_], root_]:= Module[{spec}
+FieldControllerRemove[form_?FormQ[field_], root_]:= Module[{spec}
 , spec = form[[1]] 
-; spec[[field, "Controllers"]] = DeleteCases[root] @ Lookup[ spec[field], "Controllers", {}]
+; spec[[field, "Controllers"]] = DeleteCases[root] @ form[field,"Controllers"]
 ; form = Form @ spec
 ]
 
